5a699c641f5b9fe854b472b87ea90f41ae0de7e8
Quinn Anderson <quinnrhys89@gmail.com>
2018-03-29 11:08:54 -0400

    created function to highlight quiz answer

--- main.js
+++ main.js
@@ -2,153 +2,91 @@ const elaine = document.getElementById('elaine');
 const kramer = document.getElementById('kramer');
 const jerry = document.getElementById('jerry');
 const george = document.getElementById('george');
+const newman = document.getElementById('newman');
+const frank = document.getElementById('frank');
+const quoteButton = document.querySelector('#quote-btn');
 
-// Comments
-// Git Issue
+// event listener for quote button
+quoteButton.addEventListener('click', function(e){
+    getQuote();
 
-// Event Listener for Elaine Benice
-elaine.addEventListener("click", function (e) {
-    console.log("elaine was clicked");
+});
 
+// Function to Generate random quote from API and update onto page
+function getQuote(){
+    // Ajax call to Seinfeld API
     $.ajax({
         url: "https://seinfeld-quotes.herokuapp.com/quotes",
         dataType: "JSON",
         method: "GET"
     }).then(function (res) {
         console.log(res);
+
         // variable made to reference the data from the entire array
         const allQuotes = res.quotes;
         // Used filter method to iterate over all quotes and select ones authored by Elaine
-        const elaineQuotes = allQuotes.filter(quote => quote.author === "Elaine");
+        const selectedQuotes = allQuotes.filter(quote => quote.author === "Elaine" || quote.author === "Jerry" || quote.author === "Kramer" || quote.author === "George" || quote.author === "Newman" || quote.author === "Frank Costanza");
         // used the Math.floor/Math.random methods to randomize the index numbers from the filtered results
-        const random = Math.floor(Math.random() * (elaineQuotes.length));
-        // created a finalQuote variable to store the randomized index that references a quote
-        const randomQuote = elaineQuotes[random].quote;
-        const season = elaineQuotes[random].season;
-        const episode = elaineQuotes[random].episode;
-        const finalQuote = `${randomQuote} - Elaine Benice Season ${season}, Episode ${episode}`;
+        const random = Math.floor(Math.random() * (selectedQuotes.length));
 
-    
-        // const image = elaineQuotes[random].image;
-        // if (image !== ''){
-        //         document.getElementsByTagName('img').src = image;
-        //         console.log(image);
-        // }
+        const randomQuote = selectedQuotes[random].quote;
+        const character = selectedQuotes[random].author;
+        const season = selectedQuotes[random].season;
+        const episode = selectedQuotes[random].episode;
+        const finalQuote = `${randomQuote} - ${character} Season ${season}, Episode ${episode}`;
 
-        function getResult(){
-            document.getElementById('elaine').classList.add('active');
+        console.log(character);
+
+        // function to highlight answer
+        function showAnswer(param) {
+            
+            const lowercaseCharacter = param.toLowerCase();
+            document.querySelector(`#${lowercaseCharacter}`).classList.add('result'); 
+
+            console.log(lowercaseCharacter);
         }
 
-        setTimeout(getResult,3000);
-    
-
-        
-        // find the 'text' id and update text using the finalQuote variable
         document.getElementById('text').innerHTML = finalQuote;
-        
+        setTimeout(showAnswer(character), 3000);
     });
-});
-
-
-// Event Listener for Kramer 
-kramer.addEventListener("click", function (e) {
-    console.log("kramer was clicked");
-    $.ajax({
-        url: "https://seinfeld-quotes.herokuapp.com/quotes",
-        dataType: "JSON",
-        method: "GET"
-    }).then(function (res) {
-        // variable made to reference the data from the entire array
-        const allQuotes = res.quotes;
-        // Used filter method to iterate over all quotes and select ones authored by Elaine
-        const kramerQuotes = allQuotes.filter(quote => quote.author === "Kramer");
-        // used the Math.floor/Math.random methods to randomize the index numbers from the filtered results
-        const random = Math.floor(Math.random() * (kramerQuotes.length));
-        // created a finalQuote variable to store the randomized index that references a quote
-        const randomQuote = kramerQuotes[random].quote;
-        const season = kramerQuotes[random].season;
-        const episode = kramerQuotes[random].episode;
-        const finalQuote = `${randomQuote} - Kramer Season ${season}, Episode ${episode}`;
-
-        const image = kramerQuotes[random].image;
-        // find the 'text' id and update text using the finalQuote variable
-        document.getElementById('text').innerHTML = finalQuote;
-    });
-
-});
-
-// Event Listener for Jerry 
-jerry.addEventListener("click", function (e) {
-    $.ajax({
-        url: "https://seinfeld-quotes.herokuapp.com/quotes",
-        dataType: "JSON",
-        method: "GET"
-    }).then(function (res) {
-        // variable made to reference the data from the entire array
-        const allQuotes = res.quotes;
-        // Used filter method to iterate over all quotes and select ones authored by Elaine
-        const jerryQuotes = allQuotes.filter(quote => quote.author === "Jerry");
-        // used the Math.floor/Math.random methods to randomize the index numbers from the filtered results
-        const random = Math.floor(Math.random() * (jerryQuotes.length));
-        // created a finalQuote variable to store the randomized index that references a quote
-        const randomQuote = jerryQuotes[random].quote;
-        const season = jerryQuotes[random].season;
-        const episode = jerryQuotes[random].episode;
-        const finalQuote = `${randomQuote} - Jerry Seinfeld Season ${season}, Episode ${episode}`;
-        // find the 'text' id and update text using the finalQuote variable
-        document.getElementById('text').innerHTML = finalQuote;
-    });
-    
-});
-
-// Event Listener for George
-george.addEventListener("click", function (e) {
-    $.ajax({
-        url: "https://seinfeld-quotes.herokuapp.com/quotes",
-        dataType: "JSON",
-        method: "GET"
-    }).then(function (res) {
-        // variable made to reference the data from the entire array
-        const allQuotes = res.quotes;
-        // Used filter method to iterate over all quotes and select ones authored by Elaine
-        const georgeQuotes = allQuotes.filter(quote => quote.author === "Jerry");
-        // used the Math.floor/Math.random methods to randomize the index numbers from the filtered results
-        const random = Math.floor(Math.random() * (georgeQuotes.length));
-        // created a finalQuote variable to store the randomized index that references a quote
-        const randomQuote = georgeQuotes[random].quote;
-        const season = georgeQuotes[random].season;
-        const episode = georgeQuotes[random].episode;
-        const finalQuote = `${randomQuote} - George Costanza Season ${season}, Episode ${episode}`;
-        // find the 'text' id and update text using the finalQuote variable
-        document.getElementById('text').innerHTML = finalQuote;
-    });
-});
-
-// Event Listener for Other Characters
-other.addEventListener("click", function (e) {
-    $.ajax({
-        url: "https://seinfeld-quotes.herokuapp.com/quotes",
-        dataType: "JSON",
-        method: "GET"
-    }).then(function (res) {
-        // variable made to reference the data from the entire array
-        const allQuotes = res.quotes;
-        // Used filter method to iterate over all quotes and select ones authored by Elaine
-        const otherQuotes = allQuotes.filter(quote => quote.author !== "Jerry" && quote.author !== "Elaine" && quote.author !== "George" && quote.author !== "Kramer");
-        // used the Math.floor/Math.random methods to randomize the index numbers from the filtered results
-        const random = Math.floor(Math.random() * (otherQuotes.length));
-        // created a finalQuote variable to store the randomized index that references a quote
-        const randomQuote = otherQuotes[random].quote;
-        const season = otherQuotes[random].season;
-        const episode = otherQuotes[random].episode;
-        const finalQuote = `${randomQuote} - ${otherQuotes[random].author} Season ${season}, Episode ${episode}`;
-        // find the 'text' id and update text using the finalQuote variable
-        document.getElementById('text').innerHTML = finalQuote;
-    });
-});
+}
 
 
 
+
+
+
+// function to add active class to user selection
+function selectChoice(e){
+    // console.log("selected");
+    console.log(e.target);
+    // if a button is selected (e.target) then loop through all of the children (buttons) and remove the class of active
+    if(e.target){
+        for(let i = 0; i < e.target.parentNode.children.length; i++){
+            e.target.parentNode.children[i].classList.remove('active');
+        }
+    }
+    // add the class of active to selected button
+    e.target.classList.add('active');
+}
+
+
+
+elaine.addEventListener("click", selectChoice);
+george.addEventListener("click", selectChoice);
+jerry.addEventListener("click", selectChoice);
+kramer.addEventListener("click", selectChoice);
+newman.addEventListener("click", selectChoice);
+frank.addEventListener("click", selectChoice);
+
+
+
+
+// function getResult() {
+//     document.getElementById('elaine').classList.add('active');
+// }
+
+// setTimeout(getResult, 3000);
 
 
 
